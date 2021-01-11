11aad4bc53cc8af26845965ac16f0208a353ef70
Rosellen, Peter <peter.rosellen@cgi.com>
2021-01-11 20:36:59 +0100

    A little customization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,32 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Demo - Recipes",
+    title: 'Gatsby Demo - Recipes',
+    siteUrl: 'https://gatsby.recipes',
+    description: `It's this ... or McDonals.`,
+    twitter: '@p_rosellen',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-remark",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
